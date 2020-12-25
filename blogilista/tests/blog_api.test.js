04ff0305f3b0f01225e1e692f291a21fb87b17e4
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-25 22:16:31 +0200

    4.10 posti asyncilla ja testit luonnille

--- blogilista/tests/blog_api.test.js
+++ blogilista/tests/blog_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const Blog = require('../models/blog')
@@ -7,6 +8,9 @@ const exampleBlogs = require('./example_blogs')
 
 const api = supertest(app)
 const initialBlogs = exampleBlogs.removeMongoFields(exampleBlogs.listWithManyBlogs)
+const singleBlogList = exampleBlogs.removeMongoFields(exampleBlogs.listWithOneBlog)
+
+
 
 
 beforeEach(async () => {
@@ -33,6 +37,24 @@ test('blogs have id field named as id', async () => {
   response.body.forEach(blog => {
     expect(blog.id).toBeDefined()
   })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = singleBlogList[0]
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(blog => blog.title)
+  expect(contents).toContain(
+    'Go To Statement Considered Harmful'
+  )
 })
 
 
