2efb08d3d3889366e205968d1e45cbfee5be1e7b
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-25 09:24:26 +0200

    Vielä pikkuviilaus

--- blogilista/index.js
+++ blogilista/index.js
@@ -13,5 +13,5 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
