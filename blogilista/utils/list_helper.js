5641d4948f7e0bf2c027e7ccf31f148bf80345f0
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-25 11:27:45 +0200

    favorite blog ja testi siihen

--- blogilista/utils/list_helper.js
+++ blogilista/utils/list_helper.js
@@ -11,7 +11,20 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return false
+  }
+  blogs.sort((blog1, blog2) => blog2.likes - blog1.likes )
+  return {
+    title: blogs[0].title,
+    author: blogs[0].author,
+    likes: blogs[0].likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
