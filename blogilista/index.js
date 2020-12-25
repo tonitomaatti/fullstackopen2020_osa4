33ca193bdfd58e82bb42749d45fa58f15f7302fe
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-25 09:22:45 +0200

    Pientä siivousta

--- blogilista/index.js
+++ blogilista/index.js
@@ -6,14 +6,12 @@ const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
-const PORT = config.PORT
-app.listen(PORT, () => {
+app.listen(config.PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
