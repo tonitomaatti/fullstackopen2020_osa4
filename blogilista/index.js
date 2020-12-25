c0ab7a36a47f90b724cc48ebb2151e8870aee9c5
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-25 09:33:05 +0200

    Eroteltiin app ja index

--- blogilista/index.js
+++ blogilista/index.js
@@ -1,17 +1,10 @@
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
