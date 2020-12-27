15646e9a29c2a881f937c48544bbfcefbed2bf88
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-27 05:20:49 +0200

    4.18+4.19 token ja blogit tokenilla

--- blogilista/app.js
+++ blogilista/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
