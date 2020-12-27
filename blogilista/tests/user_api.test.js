28204762deab5c26157b71915590e9e6797b6880
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-27 03:03:25 +0200

    uniikki username ja testi

--- blogilista/tests/user_api.test.js
+++ blogilista/tests/user_api.test.js
@@ -51,6 +51,27 @@ describe('when there is initially one user at db', () => {
 
     expect(response.body).toHaveLength(1)
   })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'root',
+      password: 'secret_password',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
 })
 
 afterAll(() => {
