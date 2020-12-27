f962f0d3aa899a6bde31a3af21456a5e1bb53540
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-27 02:34:14 +0200

    uuden käyttäjän luonti

--- blogilista/app.js
+++ blogilista/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -14,6 +15,7 @@ app.use(express.json())
 
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
