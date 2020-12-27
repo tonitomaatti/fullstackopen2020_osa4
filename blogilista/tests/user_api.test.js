d12e8ea070dff5ec26aee2c0542ece3349558a0c
tonitomaatti <toni.turpeinen@helsinki.fi>
2020-12-27 02:45:50 +0200

    käyttäjän luominen testi

---
+++ blogilista/tests/user_api.test.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', name: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'new_user',
+      name: 'New User',
+      password: 'secret_password',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
